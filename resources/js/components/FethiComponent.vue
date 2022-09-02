35c92b5a15689a6f0b71c34f88a1af1cc610f8f0
kaderdashing <92338093+kaderdashing@users.noreply.github.com>
2022-09-02 03:08:12 +0200

    1

---
+++ resources/js/components/FethiComponent.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <div>
+    wesha fethi
+   
+    <Lewl></Lewl>
+    <div>{{steps[0].__file}}</div>
+    <div>{{step}}</div>
+    <component v-bind:is="steps[step]"></component>
+    
+
+  </div>
+</template>
+
+<script >
+  import Zawej from './zawej.vue';
+  import { ref } from 'vue';
+  
+  import Lewl from './lewl.vue' ;
+  import { data } from 'browserslist';
+  const steps =[Zawej ,Lewl] ;
+  export default {
+      components: { Zawej ,Lewl } ,
+      setup (props){
+        
+        return{
+          steps,step
+        }
+      }
+  }
+
+  const step = ref(1);
+
+
+  console.log(steps[0].__file) ;
+  </script>
+
+
+
+<style>
+
+</style>
---
+++ resources/js/components/zawej.vue
@@ -0,0 +1,13 @@
+<template>
+    <div> zawej </div>
+    </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
